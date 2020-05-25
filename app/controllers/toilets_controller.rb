37b92d66376894f206e17b060994b1c6d7a77d93
Helene van Berchem <h.v.b@hotmail.com>
2020-05-25 18:08:24 +0200

    created toilet route

---
+++ app/controllers/toilets_controller.rb
@@ -0,0 +1,29 @@
+class ToiletsController < ApplicationController
+def index
+    @toilets = Toilet.all
+  end
+
+  def show
+    @toilet = Toilet.find(params[:id])
+  end
+
+  def new
+    @toilet = Toilet.new
+  end
+
+  def create
+    @toilet = Toilet.new(toilet_params)
+    raise
+    if @toilet.save
+      redirect_to toilets_path(@toilet)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def toilet_params
+    params.require(:toilet).permit(:name, :address, :description, :price)
+  end
+end
