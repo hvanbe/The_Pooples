9fdbccaf1d1c2bf6f4409cb046338222fefd1ede
Blaise Popote <blaisepopote@gmail.com>
2020-05-26 10:02:02 +0200

    nav

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,6 +50,12 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = false
 
+# Ensure you have defined default url options in your environments files. Here
+# is an example of default_url_options appropriate for a development environment
+#  in config/environments/development.rb:
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
