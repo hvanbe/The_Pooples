9624a6c96ab0f22f176affb229042c07c45a22da
Blaise Popote <blaisepopote@gmail.com>
2020-05-28 15:15:03 +0200

    functional datepicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import "../plugins/flatpickr";
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 
@@ -39,4 +39,6 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+
 
