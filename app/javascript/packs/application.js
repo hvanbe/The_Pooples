bdfd4e7b1d238b659675780e80945a41b7b6e1b2
Alexandre Bouvier <alexandre.b2506@gmail.com>
2020-05-28 12:20:56 +0200

    maps

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,10 +27,10 @@ import "bootstrap";
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
-initAutocomplete();
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
+  initAutocomplete();
 })
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
